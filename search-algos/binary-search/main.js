54bd2c3d71a335fb97b1a95ea27443b13031ab88
Arno Volts <thebluehermit@protonmail.com>
2022-02-02 10:42:49 +0100

    Code refactor

--- search-algos/binary-search/main.js
+++ search-algos/binary-search/main.js
@@ -2,24 +2,20 @@ var search = function(nums, target) {
     start = 0;
     end = nums.length - 1;
     
-    while (true){
-        middle  = Math.floor((end + start) / 2);
+    while (start <= end){
+        middle  = Math.trunc((end + start) / 2);
         
         if (target === nums[middle]){
             return middle;
         }
         
-        if (start === end) {
-            return -1;
-        }
-        
         if (target < nums[middle]){
-            end = middle;
-            continue;
-        }else if(target > nums[middle]){
+            end = middle - 1;
+        }else{
             start = middle + 1;
-            continue;
         }
         
     }
+    
+    return -1;
 };
